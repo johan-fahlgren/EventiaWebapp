feff8377bfc2f7ece4c58fe6be7c2cb09ec553fe
Johan Fahlgren <johan.fahlgren@utb.ecutbildning.se>
2022-03-15 12:20:27 +0100

    Created Eventia webapp MVC and Razor folder structure.

---
+++ EventiaWebapp/Controllers/EventsC.cs
@@ -0,0 +1,28 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventiaWebapp.Controllers
+{
+    public class EventsC : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Booked()
+        {
+            return View();
+        }
+
+        public IActionResult Join(Guid id)
+        {
+            return View(id);
+        }
+
+        public IActionResult Confirmation(Guid id)
+        {
+            return View(id);
+        }
+    }
+}
--- EventiaWebapp/Program.cs
+++ EventiaWebapp/Program.cs
@@ -1,6 +1,46 @@
+// DATABASE
+DataLayer.Backend.Admin.PredTestDatabase();
+
+
+//WEBAPP
+
+//PHASE 1 - configuring of webapp
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+
+
+//PHASE 2 - Middleware pipeline (Configure the HTTP request pipeline).
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
 
+app.MapControllerRoute(
+    name: "overview",
+    pattern: $"events/confirmation/{id:int}",
+    defaults: new { controller = "Events", action = "Confirmation" });
+
+app.MapControllerRoute(
+    name: "overview",
+    pattern: $"events/join/{id:int}",
+    defaults: new { controller = "Events", action = "Join" });
+
+app.MapControllerRoute(
+    name: "booked",
+    pattern: "events/booked",
+    defaults: new { controller = "Events", action = "Booked" });
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Events}/{action=index}"); // = default values.
+
+app.MapRazorPages();
+
+//PHASE 3  - Server started
 app.Run();
+}
+
+
