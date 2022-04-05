d5c731c6aeae86c9ae41c6b23b9029856f95ca1b
Johan Fahlgren <johan.fahlgren@utb.ecutbildning.se>
2022-04-05 16:54:36 +0200

    code cleanup

--- DataLayer/Data/EventiaDbContext.cs
+++ DataLayer/Data/EventiaDbContext.cs
@@ -7,27 +7,13 @@ namespace DataLayer.Data
 {
     public class EventiaDbContext : IdentityDbContext<EventiaUser, IdentityRole, string>
     {
-        //public DbSet<Attendee>? Attendees { get; set; }
         public DbSet<Event?>? Events { get; set; }
-        //public DbSet<Organizer>? Organizers { get; set; }
+
 
 
         public EventiaDbContext(DbContextOptions options) : base(options) { }
 
-        /*
-        protected void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Event>()
-                .HasOne(e => e.attendee)
-                .WithMany(e => Events)
-                .OnDelete(DeleteBehavior.SetNull);
 
-            modelBuilder.Entity<Event>()
-                .HasOne(e => e.organizer)
-                .WithMany(o => Events)
-                .OnDelete(DeleteBehavior.SetNull);
-        }
-        */
 
 
 
